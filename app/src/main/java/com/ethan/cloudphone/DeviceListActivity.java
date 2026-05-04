8bea1f0c5e9102efae94ffe6c0bd38e8ff1c8422
cloudswave <764724624@qq.com>
2026-05-04 13:28:15 +0000

    fix: 添加Log import

--- app/src/main/java/com/ethan/cloudphone/DeviceListActivity.java
+++ app/src/main/java/com/ethan/cloudphone/DeviceListActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
